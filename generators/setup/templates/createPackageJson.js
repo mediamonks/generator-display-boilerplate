a7a72caa22c950550a77e42c3bd8e7df53c7e51a
Paulie <paulie@mediamonks.com>
2022-04-15 16:40:15 +0200

    updated display-dev-server

--- generators/setup/templates/createPackageJson.js
+++ generators/setup/templates/createPackageJson.js
@@ -15,7 +15,7 @@ module.exports = function createPackageJson({ name = 'display-temple' }) {
     },
     license: 'ISC',
     dependencies: {
-      "@mediamonks/display-dev-server": "^9.3.1",
+      "@mediamonks/display-dev-server": "^9.3.2",
       "@mediamonks/display-temple": "^6.2.0",
       "@mediamonks/display-upload": "^1.5.7",
       "webfontloader": "^1.6.28"
