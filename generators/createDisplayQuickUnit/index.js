1281223b3a5e114201403de4f85da71ea6e7fd7d
Paulie <paulie@mediamonks.com>
2023-07-26 16:41:19 +0200

    Move size specific properties of richmediarc and css to size folder
    index.js > size specific folders like css get copied over to size folder
    style.css > size specific properties

--- generators/createDisplayQuickUnit/index.js
+++ generators/createDisplayQuickUnit/index.js
@@ -7,29 +7,28 @@ module.exports = class extends Generator {
   // }
 
   async action() {
-    this.fs.copy(
-      this.templatePath('shared'),
-      this.destinationPath(path.join(this.options.outputPath, 'shared'))
-    );
+    this.fs.copy(this.templatePath('shared'), this.destinationPath(path.join(this.options.outputPath, 'shared')));
 
     this.fs.copy(
       this.templatePath('shared/.sharedrc'),
-      this.destinationPath(path.join(this.options.outputPath, 'shared/.sharedrc'))
+      this.destinationPath(path.join(this.options.outputPath, 'shared/.sharedrc')),
     );
 
     const sourceConfig = this.fs.readJSON(this.templatePath('__size__/.richmediarc'));
 
-    this.options.set.forEach(size => {
+    this.options.set.forEach((size) => {
       const [width, height] = size.split('x');
 
       const outputPath = this.destinationPath(path.join(this.options.outputPath, size));
 
+      this.fs.copy(this.templatePath('__size__'), outputPath);
+
       const entry = {
-        ...sourceConfig.settings.entry
+        ...sourceConfig.settings.entry,
       };
 
       const content = {
-        ...sourceConfig.content
+        ...sourceConfig.content,
       };
 
       let config = deepmerge(sourceConfig, {
@@ -40,14 +39,12 @@ module.exports = class extends Generator {
             height: parseInt(height, 10),
           },
         },
-        content
+        content,
       });
 
       this.fs.writeJSON(path.join(outputPath, '.richmediarc'), config);
     });
 
     this.fs.delete('.yo-rc.json');
-
   }
-
 };
