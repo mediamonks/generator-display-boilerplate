f6171c2d6f1d63fd77b7279aef1f9360e25b4697
Pablo Carreira <pablog.carreira@gmail.com>
2025-02-28 16:21:34 -0300

    hasInitialSetup fix for thew new index.js

--- util/hasInitialSetup.js
+++ util/hasInitialSetup.js
@@ -1,21 +1,20 @@
 module.exports = function hasInitialSetup(ctx) {
-  if (!ctx.fs.exists(ctx.destinationPath('package.json'))) {
-    return false;
+  try {
+    // Only check if package.json exists when we're in the action phase
+    if (ctx && ctx.fs && ctx.destinationPath) {
+      if (!ctx.fs.exists(ctx.destinationPath('package.json'))) {
+        return false;
+      }
+
+      const json = ctx.fs.readJSON(ctx.destinationPath('package.json'));
+      if (!json || !json.dependencies || !json.scripts) {
+        return false;
+      }
+
+      return true;
+    }
+    return true; // Return true during initialization
+  } catch (error) {
+    return true; // Return true if we can't check yet
   }
-
-  const json = ctx.fs.readJSON(ctx.destinationPath('package.json'));
-
-  if (!json || !json.dependencies || !json.scripts) {
-    return false;
-  }
-
-  if (
-    !json.dependencies['@mediamonks/display-dev-server'] ||
-    !json.scripts.dev ||
-    !json.scripts.build
-  ) {
-    return false;
-  }
-
-  return true;
 };
